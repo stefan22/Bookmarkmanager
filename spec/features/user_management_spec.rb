da1c8e6dc081c8e7c73738a51cf641a22d5b0184
Stefan Lissia <stefan.emailacct@gmail.com>
2015-05-20 15:17:37 +0100

    completed stage 8 part1:signin in

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -20,7 +20,6 @@ feature 'User signs up' do
   end
 
 
-
   scenario 'with a password that does not match' do
     expect { sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
     expect(current_path).to eq('/users')
@@ -46,17 +45,36 @@ feature 'User signs up' do
   end
 
 
+end
 
 
+feature 'User signs in' do
 
+  before(:each) do
+    User.create(email: 'test@test.com',
+                password: 'test',
+                password_confirmation: 'test')
+  end
 
+  scenario 'with correct credentials' do
+    visit '/'
+    expect(page).not_to have_content('Welcome, test@test.com')
+    sign_in('test@test.com', 'test')
+    expect(page).to have_content('Welcome, test@test.com')
+  end
 
+  scenario 'with incorrect credentials' do
+    visit '/'
+    expect(page).not_to have_content('Welcome, test@test.com')
+    sign_in('test@test.com', 'wrong')
+    expect(page).not_to have_content('Welcome, test@test.com')
+  end
 
-
-
-
-
-
-
+  def sign_in(email, password)
+    visit '/sessions/new'
+    fill_in 'email', with: email
+    fill_in 'password', with: password
+    click_button 'Sign in'
+  end
 
 end
